0eafcb832d4b0345188268372de00b3f89036f9f
S-Amman-W <waheed2002.sw@gmail.com>
2021-01-10 00:35:40 -0500

    updated pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
 # pcost.py
 #
 # Exercise 1.27
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f)
+total = 0
+for line in f:
+    rows = line.split(',')
+    total = total + (float(rows[1]) * float(rows[2]))
+
+print('Total cost', (round(total, 2)))
