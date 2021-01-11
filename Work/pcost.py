e35db3bc945d026cdb880c606852704839bf431e
S-Amman-W <waheed2002.sw@gmail.com>
2021-01-10 22:25:08 -0500

    updated pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,11 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
-f = open('Data/portfolio.csv', 'rt')
-headers = next(f)
-total = 0
-for line in f:
-    rows = line.split(',')
-    total = total + (float(rows[1]) * float(rows[2]))
+def portfolio_cost(filename):
+    f = open(filename, 'rt')
+    headers = next(f)
+    total = 0
+    for line in f:
+        rows = line.split(',')
+        total = total + (float(rows[1]) * float(rows[2]))
+    return round(total, 2))
+    
+print('Total cost', (portfolio_cost('Data/portfolio.csv'))
 
-print('Total cost', (round(total, 2)))
